7b5e42b9c08d2b3fc5b5c54dfc6a549f71992308
Paul Murff <titan.murff@gmail.com>
2018-11-24 17:06:56 -0700

    Create InputValidator.js

---
+++ InputValidator.js
@@ -0,0 +1,40 @@
+use a validate prop boolean to send down to run or not on all forms ect
+verifyEmail(value) {
+    var emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRex.test(value)) {
+      return true;
+    }
+    return false;
+  }
+  // function that verifies if a string has a given length or not
+  verifyLength(value, length) {
+    if (value.length >= length) {
+      return true;
+    }
+    return false;
+  }
+
+   // function that verifies if two strings are equal
+   compare(string1, string2) {
+    if (string1 === string2) {
+      return true;
+    }
+    return false;
+  }
+  // function that verifies if value contains only numbers
+  verifyNumber(value) {
+    var numberRex = new RegExp("^[0-9]+$");
+    if (numberRex.test(value)) {
+      return true;
+    }
+    return false;
+  }
+  // verifies if value is a valid URL
+  verifyUrl(value) {
+    try {
+      new URL(value);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
